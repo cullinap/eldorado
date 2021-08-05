a1048432ca2d0472d24ae5539d5102e0087492e1
cullinap <cullinap@gmail.com>
2021-08-04 21:24:38 -0400

    add getter and setter test

--- src/tower.py
+++ src/tower.py
@@ -5,49 +5,49 @@ class Tower(ABC):
     def legs(self):
         pass
 
-#    @property
-#    @abstractmethod
-#    def towerNumber(self):
-#        pass
-#
-#    @towerNumber.setter
-#    @abstractmethod
-#    def towerNumber(self, value):
-#        pass
+    @property
+    @abstractmethod
+    def towerNumber(self):
+        pass
+
+    @towerNumber.setter
+    @abstractmethod
+    def towerNumber(self, value):
+        pass
 
 class Tangent(Tower):
     
-#    def __init__(self, tower_number):
-#        self.__tower_number = tower_number
+    def __init__(self, tower_number):
+        self.__tower_number = tower_number
 
     def legs(self, legType):
         print(f'Tangent leg type {legType} is created')
     
-#    @property
-#    def towerNumber(self):
-#        return self.__tower_numer
-#
-#    @towerNumber.setter
-#    def towerNumber(self, value):
-#        self.__tower_number = value
+    @property
+    def towerNumber(self):
+        return self.__tower_number
+
+    @towerNumber.setter
+    def towerNumber(self, value):
+        self.__tower_number = value
 
 class DeadEnd(Tower):
 
-#    def __init__(self, tower_number):
-#        self.__tower_number = tower_number
+    def __init__(self, tower_number):
+        self.__tower_number = tower_number
 
     def legs(self, legType):
         print(f'DeadEnd leg type {legType} is created')
 
-#    @property
-#    def towerNumber(self):
-#        return self.__tower_numer
-#
-#    @towerNumber.setter
-#    def towerNumber(self, value):
-#        self.__tower_number = value
+    @property
+    def towerNumber(self):
+        return self.__tower_number
+
+    @towerNumber.setter
+    def towerNumber(self, value):
+        self.__tower_number = value
 
 class TowerBuilder(object):
     @classmethod
-    def createTower(cls, towerType, legType):
-        return towerType().legs(legType)
+    def createTower(cls, towerType, legType, tower_number):
+        return towerType(tower_number).legs(legType)
