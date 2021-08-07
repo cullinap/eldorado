4664bc8345920347af6e9a01c154b34adde39df7
cullinap <cullinap@gmail.com>
2021-08-07 08:04:02 -0400

    add tangent assembly

--- src/hardware.py
+++ src/hardware.py
@@ -3,13 +3,27 @@ from abc import ABC, abstractmethod
 class Assembly(ABC):
     @property
     @abstractmethod
-    def assembly_type(self):
+    def clamp(self):
         pass
+    
+    @clamp.setter
+    @property
+    def clamp(self):
+        pass
+
 
 class Tangent_Assembly(Assembly):
     
     def __init__(self, tower_number):
         self.__tower_number = tower_number
+
+    @property
+    def clamp(self):
+        return self.__clamp
+    
+    @clamp.setter
+    def clamp(self, value):
+        self.__clamp = value
 
 
 
