026a68cf733b35a85059cd799ff0f230db103b0e
cullinap <cullinap@gmail.com>
2021-08-28 09:21:03 -0400

    clean up tower

--- src/tower.py
+++ src/tower.py
@@ -9,44 +9,12 @@ class Tower(ABC):
     towerNumber: int = None 
     ahead: int = None
 
+    @abstractmethod
+    def add_hardware(self, number, hw_assm):
+        '''add hardware assembly'''
 
-#    @property
-#    @abstractmethod
-#    def legs(self):
-#        pass
-#
-#    @legs.setter
-#    @abstractmethod
-#    def legs(self, value):
-#        pass
-#
-#    @property
-#    @abstractmethod
-#    def towerNumber(self):
-#        pass
-#
-#    @towerNumber.setter
-#    @abstractmethod
-#    def towerNumber(self, value):
-#        pass
-
-
-# TODO make a basic tower class and then make tangent and DE inehrit
 class Tangent(Tower):
     
-#    def __init__(self, ahead=None, tower_type):
-#        self.ahead = ahead
-#        self.towerNumber = towerNumber
-        #self.__hardware = hardware
-        
-
-    def __iter__(self):
-        for item in self.__hardware:
-            yield item
-    
-#    def __str__(self):
-#        return f'{self.__hardware.shackle}'
-            
     def add_hardware(self, number, hw_assm):
         hw = []
         for i in range(number + 1):
@@ -54,35 +22,8 @@ class Tangent(Tower):
         
         self.hardware = hw    
        
-#    @property
-#    def legs(self):
-#        return self.__leg_type
-#
-#    @legs.setter
-#    def legs(self, value):
-#        self.__leg_type = value
-#    
-#    @property
-#    def towerNumber(self):
-#        return self.__tower_number
-#
-#    @towerNumber.setter
-#    def towerNumber(self, value):
-#        self.__tower_number = value
-
 class DeadEnd(Tower):
 
-#    def __init__(self, ahead=None):
-#        self.ahead = ahead
-        #self.__hardware = hardware
-    
-    def __iter__(self):
-        for item in self.__hardware:
-            yield item
-    
-#    def __str__(self):
-#        return f'{self.__hardware.shackle}'
-            
     def add_hardware(self, number, hw_assm):
         hw = []
         for i in range(number):
@@ -90,22 +31,6 @@ class DeadEnd(Tower):
         
         self.hardware = hw    
        
-#    @property
-#    def legs(self):
-#        return self.__leg_type
-#
-#    @legs.setter
-#    def legs(self, value):
-#        self.__leg_type = value
-#    
-#    @property
-#    def towerNumber(self):
-#        return self.__tower_number
-#
-#    @towerNumber.setter
-#    def towerNumber(self, value):
-#        self.__tower_number = value
-
 # TODO change legType to towerType
 class TowerBuilder(object):
     @classmethod
