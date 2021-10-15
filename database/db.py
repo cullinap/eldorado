91d3eec1496115cf2fd9277d169feb874d269c71
cullinap <cullinap@gmail.com>
2021-10-15 08:28:29 -0400

    start to create db

--- database/db.py
+++ database/db.py
@@ -1,4 +1,21 @@
 import sqlite3
 
 conn = sqlite3.connect('newdb.sqlite')
+cur = conn.cursor()
 
+table_schema = """
+CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT
+); 
+"""
+
+cur.execute(table_schema)
+
+result = cur.fetchall()
+print(result)
+
+cur.close()
+
+conn.close()
