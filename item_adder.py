f9e6baa517fecb8df65c530b3342f0f214ac6d66
cullinap <cullinap@gmail.com>
2021-10-16 07:07:08 -0400

    add db to itm adder

--- item_adder.py
+++ item_adder.py
@@ -1,6 +1,7 @@
 from src.inbound import Truck, Inventory
 from src.minimalBlock import MinimalBlock, ItemBlock, AccessoryBlock
 import json
+import sqlite3
 
 def bill_of_lading() -> dict:
     items = {}
@@ -23,17 +24,41 @@ def bill_of_lading_json(contents: dict) -> json:
     return json.dumps(contents)
 
 def store_items_in_db(shipment):
-    # conceptually store items in from this function once postgres up
-    add_item_to_db = shipment
-    return add_item_to_db
+    conn = sqlite3.connect('warehouse.sqlite')
+    cur = conn.cursor()
+    
+    table_schema = """
+    CREATE TABLE IF NOT EXISTS inventory ( 
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        description TEXT
+    );
+    """
+
+    cur.execute(table_schema) 
+
+    insert_query = """
+    INSERT INTO inventory (name, description)
+    VALUES ('item', 'qty')
+    """
+    
+    cur.execute('SELECT * FROM inventory')
+
+    result = cur.fetchall()
+    print(result)
+    
+    cur.close()
+    conn.close()
+
 
 items = bill_of_lading()
 truck_1 = truck_delivery(items)
 truck_json = bill_of_lading_json(truck_1.inventory)
 initial_block = ItemBlock("Initial String", [truck_json])
-add_to_db = store_items_in_db(initial_block)
-print(initial_block.block_hash, initial_block.block_data)
-print(truck_1.inventory)
-print(add_to_db)
+
+store_items_in_db(initial_block)
+#print(initial_block.block_hash, initial_block.block_data)
+#print(truck_1.inventory)
+#print(add_to_db)
 
 
