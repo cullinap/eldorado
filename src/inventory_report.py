6dfae6e87fc0cd8a01c27957b72ea68862fa61c9
cullinap <cullinap@gmail.com>
2021-10-28 09:24:28 -0400

    added a get_all_result

--- src/inventory_report.py
+++ src/inventory_report.py
@@ -1,5 +1,34 @@
 import sqlite3
 
+def get_all_results():
+    conn = sqlite3.connect('database/warehouse.sqlite')
+    cur = conn.cursor()
+
+    print(cur.fetchall())
+
+    conn.commit()
+    conn.close()
+
+
+
+def create_table():
+    conn = sqlite3.connect('database/warehouse.sqlite')
+    cur = conn.cursor()
+
+    add_col = """
+    ALTER TABLE inventory 
+    ADD COLUMN (?, ?);
+    """
+    
+    col = str(input('enter a col name: '))
+    col_type = input('enter a type: ')
+    
+    cur.execute(add_col, (col, col_type))
+    conn.commit()
+
+    cur.close()
+    conn.close()
+    
 
 def print_inventory():
     conn = sqlite3.connect('database/warehouse.sqlite')
@@ -25,3 +54,5 @@ def delete_all_inventory():
     conn.commit()
     cur.close()
     conn.close()
+
+
